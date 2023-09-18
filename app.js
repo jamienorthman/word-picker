02b47b24b388bca3832e078844c3ac9d3060d542
Jamienorthman <mikoann88@gmail.com>
2023-09-18 20:27:11 +0200

    added comments for clarity

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import { euphemismData } from '/data.js'
-
+import { euphemismData } from './data.js'
+//Global variables to access later:
 const categoryRadios = document.getElementById('category-radios')
 const getWordBtn = document.getElementById('get-word-btn')
 const modalInner = document.getElementById('modal-inner')
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-
+// Highlights a radio only when checked:
 categoryRadios.addEventListener('change', highlightCheckedOption)
 
 modalCloseBtn.addEventListener('click', closeModal)
@@ -24,12 +24,14 @@ function closeModal(){
     modal.style.display = 'none'
 }
 
+//Produces HTML for the randomly selected word of getSingleWordObject()
+//Euphemism is displayed, then fades out, and then its true meaning fades in.
 function renderWord(){
     const wordObject = getSingleWordObject()
     modalInner.innerHTML =  `
         <span 
         class="fade-out">
-        ${wordObject.word}
+        ${wordObject.word} 
         </span>
         <span 
         class="fade-in">
@@ -39,10 +41,12 @@ function renderWord(){
     modal.style.display = 'flex'
 }
 
+// Randomly goes through matchingWordsArray to produce one of the words that shares
+//category chosen by user
 function getSingleWordObject(){
     const wordsArray = getMatchingWordsArray()
     
-    if(wordsArray.length === 1){
+    if(wordsArray.length === 1){ //if it's just one word sharing chosen category, return that word.
         return wordsArray[0]
     }
     else{
@@ -54,14 +58,16 @@ function getSingleWordObject(){
 function getMatchingWordsArray(){     
     if(document.querySelector('input[type="radio"]:checked')){
         const selectedCategory = document.querySelector('input[type="radio"]:checked').value
-        
+        // if the category of the checked radio value...
         const matchingWordsArray = euphemismData.filter(function(word){
             return word.tags.includes(selectedCategory)
-        })
+        }) //...matches other word's categories (in array they're called "tags")...
         return matchingWordsArray 
-    }  
+    }   //...collect those words in a new array.
 }
 
+//Creates an array of categories (called "tags" in array) so each category
+//appears just once.
 function getCategoriesArray(words){
     const categoriesArray = []    
     for (let word of words){
@@ -74,6 +80,8 @@ function getCategoriesArray(words){
     return categoriesArray
 }
 
+//Using getCategoriesArray(), produces HTML (radio button elements) for each 
+//category and includes them in the Categoryradios container div.
 function renderCategoryRadios(words){
         
     let radioItems = ``
